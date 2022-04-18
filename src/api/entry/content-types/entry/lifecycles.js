b764bb9b61a1bbb5c14f10b353f1a34e2a8e5a09
Lloyd JvR <thefullstackjunkie@gmail.com>
2022-04-18 15:39:48 +0200

    added lifecycle methods to the update and create of entries to calculate the kilos per liter field

--- src/api/entry/content-types/entry/lifecycles.js
+++ src/api/entry/content-types/entry/lifecycles.js
@@ -1,7 +1,8 @@
 module.exports = {
   beforeCreate(event) {
     const { data } = event.params;
-
+    console.log(data);
+    console.log(data.tripDistance / data.liters);
     data.kilos_per_liter = data.tripDistance / data.liters;
   },
 
